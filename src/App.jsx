b0befc34be1b6f3af699be11f2a6e12ed00cc386
Benjamin Pachevie <b.pachevie@gmail.com>
2023-11-13 17:55:23 +0100

    optimizing states updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -54,7 +54,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback( function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -65,11 +65,12 @@ function App() {
       "selectedPlaces",
       JSON.stringify(storeIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
+
 
   return (
     <>
-      <Modal open={modalIsOpen}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
